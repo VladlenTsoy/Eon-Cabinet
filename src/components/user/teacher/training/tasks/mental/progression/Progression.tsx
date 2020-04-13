e332de6eec7269540f9af6072b2fe262ebd5f3b8
Vladlen <vladlengg@gmail.com>
2020-04-13 13:32:48 +0500

    update Progression.tsx anzan setting

--- src/components/user/teacher/training/tasks/mental/progression/Progression.tsx
+++ src/components/user/teacher/training/tasks/mental/progression/Progression.tsx
@@ -1,39 +1,22 @@
 import React from 'react';
-import ProgressionFormBody from "./progression-from-body/ProgressionFormBody";
-import {useFieldsSetting} from "../../../../../../../effects/use-fields-setting.effect";
+import ProgressionFormItems from "./progression-form-items/ProgressionFormItems";
+import ConfigBlock from "../../config/Config";
+import usingFormBodyLayout from "../layout/form-body/usingFormBody.layout";
 
 interface ProgressionProps {
-    userSetting?: any;
-    clearSaveSetting?: any;
-    startApplication?: (setting: any, print: boolean) => void;
-    addSettingHomework?: (setting: any) => void;
 }
 
-const Progression:React.FC<ProgressionProps> = (
-    {
-        userSetting,
-        clearSaveSetting,
-        startApplication,
-        addSettingHomework,
-    }
-) => {
-    const [fields, setFields] = useFieldsSetting({setting: userSetting});
-
-    /**
-     * Обновление настроек
-     *
-     * @param changedFields
-     */
-    const handlerChangeHeader = (changedFields: any) =>
-        setFields((prevState: any) => ({...prevState, ...changedFields}));
-
-    return <ProgressionFormBody
-        onChange={handlerChangeHeader}
-        clearSaveSetting={clearSaveSetting}
-        startApplication={startApplication}
-        addSettingHomework={addSettingHomework}
-        setting={fields}
-    />;
+const Progression:React.FC<ProgressionProps> = () => {
+    return <>
+        <ProgressionFormItems/>
+        <ConfigBlock
+            sounds={{
+                language: true,
+            }}
+            mods={{
+                plus: true, abacus: true,
+            }}/>
+    </>;
 };
 
-export default Progression;
+export default usingFormBodyLayout(Progression);
