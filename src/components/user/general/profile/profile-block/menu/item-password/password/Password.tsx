c75de1e9ce8b2a3112b79290af6f683aecff3c12
Vladlen <vladlengg@gmail.com>
2020-04-18 10:27:52 +0500

    update Password.tsx to Profile

--- src/components/user/general/profile/profile-block/menu/item-password/password/Password.tsx
+++ src/components/user/general/profile/profile-block/menu/item-password/password/Password.tsx
@@ -1,32 +1,30 @@
-import * as React from "react";
-import { LockOutlined } from '@ant-design/icons';
-import { Form } from '@ant-design/compatible';
-import '@ant-design/compatible/assets/index.css';
-import { Button, Input, message } from "antd";
+import React from "react";
+import {LockOutlined, SaveOutlined} from '@ant-design/icons';
+import {Button, Input, message, Form} from "antd";
 import {useState} from "react";
 import {useSelector} from "react-redux";
 
-const PasswordBlock = ({form, currentUser, closeModal}: any) => {
-    const {api} = useSelector((state: any) => state);
-    const {getFieldDecorator} = form;
-    const [loading, setLoading] = useState(false);
-    const [confirmDirty, setConfirmDirty] = useState(false);
+interface PasswordProps {
+    currentUser: any;
+    closeModal: () => void;
+}
 
-    const handleSubmit = (e: any) => {
-        e.preventDefault();
-        form.validateFields(async (err: any, values: any) => {
-            if (!err) {
-                setLoading(true);
-                try {
-                    await api.user_general.post(`/${currentUser.id}/password`, values);
-                    message.success('Вы успешно сменили пароль!');
-                } catch (e) {
-                    console.log(e.response.data.message);
-                    message.error(`${e.response.data.message}!`);
-                }
-                setLoading(false);
-            }
-        });
+const PasswordBlock: React.FC<PasswordProps> = ({currentUser, closeModal}) => {
+    const [form] = Form.useForm();
+    const {api} = useSelector((state: any) => state);
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = async (values: any) => {
+        setLoading(true);
+        try {
+            await api.user_general.post(`/${currentUser.id}/password`, values);
+            message.success('Вы успешно сменили пароль!');
+            closeModal();
+        } catch (e) {
+            console.log(e.response.data.message);
+            message.error(`${e.response.data.message}!`);
+            setLoading(false);
+        }
     };
 
     const compareToFirstPassword = (rule: any, value: any, callback: any) => {
@@ -36,57 +34,49 @@ const PasswordBlock = ({form, currentUser, closeModal}: any) => {
             callback();
     };
 
-    const validateToNextPassword = (rule: any, value: any, callback: any) => {
-        if (value && confirmDirty)
-            form.validateFields(['confirm'], {force: true});
-        callback();
-    };
+    return <Form onFinish={handleSubmit} className="login-form" layout="vertical" form={form}>
+        <Form.Item
+            label="Текущий пароль"
+            name="password_old"
+            rules={[{required: true, message: 'Введите текущий пароль!'}]}
 
-    const handleConfirmBlur = (e: any) => {
-        const {value} = e.target;
-        setConfirmDirty(confirmDirty || !!value);
-    };
+        >
+            <Input.Password prefix={<LockOutlined/>}/>
+        </Form.Item>
 
-    return (
-        <Form onSubmit={handleSubmit} className="login-form" layout="vertical">
-            <Form.Item label="Текущий пароль">
-                {getFieldDecorator('password_old', {
-                    rules: [{required: true, message: 'Введите текущий пароль!'}],
-                })(<Input.Password
-                    prefix={<LockOutlined style={{color: 'rgba(0,0,0,.25)'}} />}
-                />)}
-            </Form.Item>
+        <Form.Item
+            label="Новый пароль"
+            hasFeedback
+            name="password_new"
+            rules={[
+                {required: true, message: "Введите пароль!"},
+                {min: 5, message: "Пароль должен состоять мин. из 5 символов!"},
+                {pattern: /^[A-Za-z0-9]+$/, whitespace: true, message: "Только латинский буквы и цифры!"},
+                // {validator: validateToNextPassword},
+            ]}
+        >
+            <Input.Password prefix={<LockOutlined/>}/>
+        </Form.Item>
 
-            <Form.Item label="Новый пароль" hasFeedback>
-                {getFieldDecorator('password_new', {
-                    rules: [
-                        {required: true, message: 'Введите новый пароль!',},
-                        {min: 6, message: 'Пароль должен содержать от 6 символов!',},
-                        {validator: validateToNextPassword},
-                    ],
-                })(<Input.Password
-                    prefix={<LockOutlined style={{color: 'rgba(0,0,0,.25)'}} />}
-                />)}
-            </Form.Item>
+        <Form.Item
+            label="Потвердите новый пароль"
+            hasFeedback
+            dependencies={['password_new']}
+            style={{marginBottom: '1.5rem'}}
+            rules={[
+                {required: true, message: 'Потвердите новый пароль!',},
+                {validator: compareToFirstPassword},
+            ]}
+            name="password_confirm"
+        >
+            <Input.Password prefix={<LockOutlined/>}/>
+        </Form.Item>
 
-            <Form.Item label="Потвердите новый пароль" hasFeedback style={{marginBottom: '1.5rem'}}>
-                {getFieldDecorator('password_confirm', {
-                    rules: [
-                        {required: true, message: 'Потвердите новый пароль!',},
-                        {validator: compareToFirstPassword,},
-                    ],
-                })(<Input.Password
-                    onBlur={handleConfirmBlur}
-                    prefix={<LockOutlined style={{color: 'rgba(0,0,0,.25)'}} />}
-                />)}
-            </Form.Item>
-
-            <div className="actions-block">
-                <Button type="primary" htmlType="submit" loading={loading}>Сохранить</Button>
-                <Button onClick={closeModal}>Отмена</Button>
-            </div>
-        </Form>
-    );
+        <div className="actions-block">
+            <Button type="primary" htmlType="submit" icon={<SaveOutlined/>} loading={loading}>Сохранить</Button>
+            <Button onClick={closeModal}>Отмена</Button>
+        </div>
+    </Form>;
 };
 
-export default Form.create<any>()(PasswordBlock);
+export default PasswordBlock;
