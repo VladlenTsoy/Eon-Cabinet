d138b919445a30a3fd1c1b5a653bb3ea6ac37f79
Vladlen <vladlengg@gmail.com>
2020-04-19 13:20:16 +0500

    update counter

--- src/components/user/student/homework/more/tasks/task/counter/Counter.tsx
+++ src/components/user/student/homework/more/tasks/task/counter/Counter.tsx
@@ -87,7 +87,7 @@ const Counter: React.FC<CounterProps> = ({task, type}) => {
             </CounterWrapper>
             {task.second ?
                 <SecondCounterWrapper type={task.second.exodus ? 'winner' : 'loser'}>
-                    <span className="quantity">{task.count_all} : {task.second.count_success}</span>
+                    <span className="quantity">{task.count_success} / {task.second.count_all}</span>
                 </SecondCounterWrapper> : null
             }
         </CountersWrapper>
