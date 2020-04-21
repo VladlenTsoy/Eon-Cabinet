39567b265fb6ac08fec3ca847fbdfee51936b696
Vladlen <vladlengg@gmail.com>
2020-04-21 11:50:23 +0500

    update Answer.tsx for Flash-anzan

--- src/components/user/general/tasks/mental/flash-anzan/answer/Answer.tsx
+++ src/components/user/general/tasks/mental/flash-anzan/answer/Answer.tsx
@@ -1,62 +1,86 @@
 import React from 'react';
-import { Form } from '@ant-design/compatible';
-import '@ant-design/compatible/assets/index.css';
-import { Button, Col, Input, Row, Typography } from "antd";
+import {Button, Col, Row, Typography, Form, InputNumber} from "antd";
 import {useDispatch, useSelector} from "react-redux";
-import {Card} from "lib";
+import {Card, FormItem} from "lib";
 import {gameChangeStats, gameChangeStatus, gameChangeTotals} from "store/game/actions";
+import {ArrowRightOutlined} from '@ant-design/icons';
+import styled from "styled-components";
 
 const {Title} = Typography;
 
-const Answer: React.FC<any> = ({form}) => {
+const CardWrapper = styled(Card)`
+  &.ant-card{
+    text-align: center;
+    
+    .ant-card-body{
+      .ant-typography{
+        margin-bottom: 1rem;
+      }  
+      
+      padding: 1.5rem;
+    }
+  }
+`;
+
+const InputWrapper = styled(InputNumber)`
+  width: 100%;
+  
+  .ant-input-number-input{
+    text-align: center;
+  }
+`;
+
+const Answer: React.FC = () => {
     const {game} = useSelector((state: any) => state);
-    const {getFieldDecorator} = form;
     const dispatch = useDispatch();
     const {totals, stats} = game;
 
-    const handleSubmit = (e: any) => {
-        e.preventDefault();
-        form.validateFields(async (err: any, values: any) => {
-            if (!err) {
-                let _stats = stats;
-                let _totals = totals.map((total: any, key: number) => {
-                    if (total.answer === Number(values.user[key]))
-                        _stats = {..._stats, success: _stats.success + 1};
+    const onFinishSubmit = (values: any) => {
+        let _stats = stats;
+        let _totals = totals.map((total: any, key: number) => {
+            if (total.answer === Number(values.user[key]))
+                _stats = {..._stats, success: _stats.success + 1};
 
-                    return {
-                        ...total, ...{
-                            user: Number(values.user[key]),
-                            result: total.answer === Number(values.user[key])
-                        }
-                    };
-                });
-
-                dispatch(gameChangeStats(_stats));
-                dispatch(gameChangeTotals(_totals));
-                dispatch(gameChangeStatus('result'));
-            }
+            return {
+                ...total, ...{
+                    user: Number(values.user[key]),
+                    result: total.answer === Number(values.user[key])
+                }
+            };
         });
+
+        dispatch(gameChangeStats(_stats));
+        dispatch(gameChangeTotals(_totals));
+        dispatch(gameChangeStatus('result'));
     };
 
-    return <Row  justify="center" align="middle" gutter={15}>
+    return <Row justify="center" align="middle" gutter={15}>
         <Col xl={8}>
-            <Card>
-                <Form onSubmit={handleSubmit}>
-                    <Title level={3}>Введите ответ</Title>
+            <CardWrapper>
+                <Form onFinish={onFinishSubmit}>
+                    <Title level={2}>Введите ответ</Title>
                     {totals.map((total: any, key: any) =>
-                        <Form.Item key={key}>
-                            {getFieldDecorator(`user[${key}]`, {})(
-                                <Input placeholder={`Ваш ответ №${key + 1}`} autoFocus={key === 0}/>
-                            )}
-                        </Form.Item>
+                        <FormItem
+                            key={key}
+                            name={['user', key]}
+                            requiredMsg={`Введите ответ №${key + 1}!`}
+                        >
+                            <InputWrapper
+                                type="number"
+                                autoFocus={key === 0}
+                                autoComplete="off"
+                                size="large"
+                                placeholder={`Ваш ответ №${key + 1}`}
+                            />
+                        </FormItem>
                     )}
-                    <Button type="primary" htmlType="submit" block>
+                    <Button type="primary" htmlType="submit" size="large" block icon={<ArrowRightOutlined/>}>
                         Далее
                     </Button>
                 </Form>
-            </Card>
+            </CardWrapper>
         </Col>
     </Row>;
 };
 
-export default Form.create<any>()(Answer);
+export default Answer;
