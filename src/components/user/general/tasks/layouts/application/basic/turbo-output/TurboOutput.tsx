c562ee49548c72ac4d80f793745e968ba8ce65e4
Vladlen <vladlengg@gmail.com>
2020-05-12 16:08:27 +0500

    update deps for teacher application

--- src/components/user/general/tasks/layouts/application/basic/turbo-output/TurboOutput.tsx
+++ src/components/user/general/tasks/layouts/application/basic/turbo-output/TurboOutput.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React from 'react';
 import styled from "styled-components";
 import TextFit
     from "../../../../../../teacher/training/tasks/mental/multi-anzan/exercise-setting/multiplication-block/TextFit";
