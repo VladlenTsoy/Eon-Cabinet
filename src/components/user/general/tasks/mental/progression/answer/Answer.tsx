1c45981924536c202e0aeb16495c7f8caa67255c
Vladlen <vladlengg@gmail.com>
2020-05-13 19:52:37 +0500

    update Answer.tsx to progression

--- src/components/user/general/tasks/mental/progression/answer/Answer.tsx
+++ src/components/user/general/tasks/mental/progression/answer/Answer.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import {useSelector} from "react-redux";
 import AnswerLayout from "../../../layouts/answer/Answer.layout";
 import FormInputAnswerLayout from "../../../layouts/answer/form-input-answer/FormInputAnswer.layout";
-import {Button, Typography} from "antd";
-import {ArrowRightOutlined} from '@ant-design/icons';
-
-const {Title} = Typography;
+import {game} from "../../../../../../../store/game/reducer";
+import {totalsSelect} from "../../../../../../../store/tasks/totals/reducer";
 
 const Answer = () => {
-    const {game} = useSelector((state: any) => state);
-    const {totals, stats, currentTimes} = game;
+    const {stats, currentTimes} = useSelector(game);
+    const totals = useSelector(totalsSelect);
 
     const onFinishSubmit = (values: any) => {
         let result = totals[currentTimes].answer === Number(values.answer);
@@ -22,11 +20,7 @@ const Answer = () => {
 
 
     return <AnswerLayout cols={{xl: 10, md: 12, sm: 14, xs: 24}} checkHandler={onFinishSubmit}>
-        <Title level={2}>Введите ответ</Title>
         <FormInputAnswerLayout index={1} autoFocus={1}/>
-        <Button type="primary" htmlType="submit" size="large" block icon={<ArrowRightOutlined/>}>
-            Далее
-        </Button>
     </AnswerLayout>;
 };
 
