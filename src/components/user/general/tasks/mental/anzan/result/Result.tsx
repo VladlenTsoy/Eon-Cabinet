61f5dc48403d8c7c568cb55608d2e011af9f77be
Vladlen <vladlengg@gmail.com>
2020-05-16 18:58:13 +0500

    update result to anzan

--- src/components/user/general/tasks/mental/anzan/result/Result.tsx
+++ src/components/user/general/tasks/mental/anzan/result/Result.tsx
@@ -4,18 +4,56 @@ import BasicResult from "./basic-result/Basic";
 import DoubleResult from "./double-result/Double";
 import {totalsSelect} from "../../../../../../../store/tasks/totals/reducer";
 import {settingAnzan} from "../../../../../../../store/tasks/setting/reducer";
+import styled from "styled-components";
+
+const ResultStyle = styled.div`
+  > div {
+    display: grid;
+    grid-template-columns: 50px 100px 175px 175px 1fr;
+    gap: 1rem;
+    border-bottom: 1px solid ${props => props.theme.light_color_border};
+    
+    > div {
+      padding: 0.5rem;
+    }
+    
+    > div:not(:first-child):not(:last-child){
+      text-align: center;
+    }
+  }
+  
+  .header {
+    color: ${props => props.theme.color_second};
+    font-size: 16px;
+  }
+  
+  .warning { color: ${props => props.theme.color_warning}; }
+  .success { color: ${props => props.theme.color_success}; }
+  .danger { color: ${props => props.theme.color_danger}; }
+`;
 
 const ResultBlock: React.FC = () => {
     const totals: any = useSelector(totalsSelect);
     const setting = useSelector(settingAnzan);
     const isMultiplication = setting.mode === 'divide' || setting.mode === 'multiply';
-    return totals.map((total: any, key: number) =>
-        setting.anzan === 'double' ?
-            <DoubleResult total={total} setting={setting} isMultiplication={isMultiplication} key={key}
-                          keyExercise={key}/> :
-            <BasicResult total={total} setting={setting} isMultiplication={isMultiplication} key={key}
-                         keyExercise={key}/>);
 
+    return <ResultStyle>
+        <div className="header">
+            <div>#</div>
+            <div>Результат</div>
+            <div>Правильный ответ</div>
+            <div>Ваш ответ</div>
+            <div>Задание</div>
+        </div>
+        {
+            totals.map((total: any, key: number) =>
+                setting.anzan === 'double' ?
+                    <DoubleResult total={total} setting={setting} isMultiplication={isMultiplication} key={key}
+                                  keyExercise={key}/> :
+                    <BasicResult total={total} setting={setting} isMultiplication={isMultiplication} key={key}
+                                 keyExercise={key}/>)
+        }
+    </ResultStyle>
 };
 
 export default ResultBlock;
--- src/components/user/general/tasks/mental/anzan/result/basic-result/Basic.tsx
+++ src/components/user/general/tasks/mental/anzan/result/basic-result/Basic.tsx
@@ -1,27 +1,30 @@
 import React from 'react';
 import styled from "styled-components";
-import {CardWrapper} from "../../../../layouts/result/homework/_new/details/card/Card";
-import { TrophyOutlined } from '@ant-design/icons';
+import {TrophyOutlined} from '@ant-design/icons';
 
-const BasicResultWrapper = styled(CardWrapper)`
-  .card{
-    grid-template-columns: 50px 1fr 1fr 1fr;
+const BasicResultWrapper = styled.div`
+  .number {
+    color: ${props => props.theme.color_second};
+    font-size: 30px;
+  }
+  .trophy {
+    font-size: 30px;
+  }
+  
+  .answer, .user{
+    font-size: 30px;  
+  }
 
-    &.multi{
-      grid-template-columns: 50px 1fr 1fr 1fr 1fr;
-    }
-
-    .exercises{
-      font-size: 30px;
-      grid-column-start: 1;
-      grid-column-end: 5;
-      white-space: nowrap;
-      overflow-x: auto;
-      overflow-y: hidden;
-    }
-    .exercises-multiplication{
-      font-size: 50px;
-    }
+  .exercises{
+    font-size: 30px;
+    white-space: nowrap;
+    overflow-x: auto;
+    overflow-y: hidden;
+    background: ${props => props.theme.color_border}33;
+  }
+  
+  .exercises-multiplication{
+    font-size: 50px;
   }
 `;
 
@@ -33,34 +36,30 @@ interface BasicResultProps {
 }
 
 const BasicResult: React.FC<BasicResultProps> = ({total, isMultiplication, keyExercise, setting}) => {
-    return (
-        <BasicResultWrapper>
-            <div className={`card ${isMultiplication ? 'multi' : ''}`}>
-                <div className="number">
-                    #{keyExercise}
-                </div>
-                <div className={`trophy ${total.result ? 'success' : 'danger'}`}>
-                    <TrophyOutlined />
-                </div>
-                {isMultiplication ?
-                    <div className="exercises-multiplication">
-                        {total.exercise[0]}{setting.mode === 'multiply' ? ' * ' : ' / '}{total.exercise[1]}
-                    </div> : null
-                }
-                <div className="answer">
-                    {total.answer}
-                </div>
-                <div className={`user ${total.result ? 'success' : 'danger'}`}>
-                    {String(total.user) || 'Пусто'}
-                </div>
-                {!isMultiplication ?
-                    <div className="exercises">
-                        {total.exercise.join(', ')}
-                    </div> : null
-                }
-            </div>
-        </BasicResultWrapper>
-    );
+    return <BasicResultWrapper>
+        <div className="number">
+            {keyExercise + 1}
+        </div>
+        <div className={`trophy ${total.result ? 'warning' : 'danger'}`}>
+            <TrophyOutlined/>
+        </div>
+        {isMultiplication &&
+        <div className="exercises-multiplication">
+            {total.exercise[0]}{setting.mode === 'multiply' ? ' * ' : ' / '}{total.exercise[1]}
+        </div>
+        }
+        <div className="answer">
+            {total.answer}
+        </div>
+        <div className={`user ${total.result ? 'success' : 'danger'}`}>
+            {String(total.user) || 'Пусто'}
+        </div>
+        {!isMultiplication &&
+        <div className="exercises">
+            {total.exercise.join(', ')}
+        </div>
+        }
+    </BasicResultWrapper>
 };
 
 export default BasicResult;
