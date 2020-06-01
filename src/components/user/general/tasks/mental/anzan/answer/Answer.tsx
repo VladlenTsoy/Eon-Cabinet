990bc801974d70e11195afa30358d3f27c442412
vladlen <vladlengg@gmail.com>
2020-06-02 03:50:08 +0500

    update Answer.tsx to anzan

--- src/components/user/general/tasks/mental/anzan/answer/Answer.tsx
+++ src/components/user/general/tasks/mental/anzan/answer/Answer.tsx
@@ -10,59 +10,47 @@ const Answer: React.FC = () => {
     const isDouble = setting.anzan === 'double';
     const isGroup = setting.extra && setting.extra.includes('group');
 
-    const cols = isDouble ?
-        {xl: 12, md: 14, sm: 16, xs: 24} :
-        {xl: 10, md: 12, sm: 14, xs: 24};
+    const a = (answer: number, user: number, success: number) => {
+        const result = answer === Number(user);
+        result && success++;
+
+        return {user: Number(user), result: result};
+    };
+
+    const b = (total: { answer: number }[], users: number[], success: number) => {
+        const result1 = total[0].answer === Number(users[0]);
+        const result2 = total[1].answer === Number(users[1]);
+        result1 && result2 && success++;
+
+        return [
+            {...total[0], user: Number(users[0]), result: result1},
+            {...total[1], user: Number(users[1]), result: result2},
+        ];
+    };
 
     const checkAnswerGroup = (values: any, success = 0) => {
         let _totals = isDouble ?
-            totals.map((total: any, key: number) => {
-                let result1 = total[0].answer === Number(values.answer1[key]);
-                let result2 = total[1].answer === Number(values.answer2[key]);
-
-                result1 && result2 && success++;
-
-                return [
-                    {...total[0], ...{user: Number(values.answer1[key]), result: result1}},
-                    {...total[1], ...{user: Number(values.answer2[key]), result: result2}}
-                ];
-            }) :
-            totals.map((total: any, key: number) => {
-                let result = total.answer === Number(values.answer[key]);
-                result && success++;
-
-                return {...total, ...{user: Number(values.answer[key]), result}};
-            });
+            totals.map((total: any, key: number) => b(total, [values.answer1[key], values.answer2[key]], success)) :
+            totals.map((total: any, key: number) => ({...total, ...a(total.answer, values.answer[key], success)}));
 
         return {totals: _totals, status: 'result', success}
     };
 
-    const checkAnswer = (values: any, success = 0, total= {}) => {
-        if (isDouble) {
-            const result1 = totals[currentTimes][0].answer === Number(values.answer1);
-            const result2 = totals[currentTimes][1].answer === Number(values.answer2);
-            result1 && result2 && success++;
-
-            total = [
-                {user: Number(values.answer1), result: result1},
-                {user: Number(values.answer2), result: result2},
-            ];
-        } else {
-            const result = totals[currentTimes].answer === Number(values.answer);
-            result && success++;
-
-            total ={user: Number(values.answer), result: result};
-        }
-
+    const checkAnswer = (values: any, success = 0, total = {}) => {
+        total = isDouble ?
+            b(totals[currentTimes], [values.answer1, values.answer2], success):
+            a(totals[currentTimes].answer, values.answer, success);
         return {status: 'intermediate', success, total}
     };
 
-    const handleSubmit = (values: any): any => isGroup ?
-        checkAnswerGroup(values) :
-        checkAnswer(values);
+    const handleSubmit = isGroup ? checkAnswerGroup : checkAnswer;
 
     return <AnswerLayout
-        cols={cols}
+        cols={
+            isDouble ?
+                {xl: 12, md: 14, sm: 16, xs: 24} :
+                {xl: 10, md: 12, sm: 14, xs: 24}
+        }
         checkHandler={handleSubmit}
     >
         {isDouble ? <Double/> : <Basic/>}
