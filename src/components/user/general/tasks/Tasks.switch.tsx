9158a2bcf60362b3420f847b5fcd689914b7b145
VladlenTsoy <vladlengg@gmail.com>
2020-06-04 17:12:16 +0500

    add props to Tasks.switch.tsx

--- src/components/user/general/tasks/Tasks.switch.tsx
+++ src/components/user/general/tasks/Tasks.switch.tsx
@@ -4,8 +4,13 @@ import {useRouteMatch} from 'react-router-dom';
 const Mental = React.lazy(() => import("./mental/Mental"));
 const Mnemonics = React.lazy(() => import("./mnemonics/Mnemonics"));
 
+interface RouteMatchProps{
+    disciplineId: string;
+    taskId: string;
+}
+
 const TasksSwitch = () => {
-    const {params} = useRouteMatch();
+    const {params} = useRouteMatch<RouteMatchProps>();
     return <>
         {params.disciplineId === '1' && <Mental taskId={params.taskId}/>}
         {params.disciplineId === '2' && <Mnemonics taskId={params.taskId}/>}
