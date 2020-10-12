d64416b16f473163e77ba147428761384c8d326d
vladlen <vladlengg@gmail.com>
2020-10-12 17:49:18 +0500

    update lib/ui

--- src/lib/ui/index.ts
+++ src/lib/ui/index.ts
@@ -1,25 +1,10 @@
-export {default as LoadingBlock} from './loading-block/LoadingBlock';
-export {default as Loader} from './loader/Loader';
 export {default as ButtonLink} from './button-link/ButtonLink';
-export {default as Card} from './card/Card';
-export {default as FormItem} from './form/form-item/FormItem';
-export {default as ModalMenu} from './modal-menu/ModalMenu';
 export {default as GrayIcon} from './gray-icon/GrayIcon';
-/*  */
 export {default as Alert} from './alert/Alert';
 export {default as Coins} from './coins/Coins';
 export {default as Spin} from './spin/Spin';
-export {default as Drawer} from './drawer/Drawer';
-export {default as InputPhoto} from './form/InputPhoto';
-export {default as InputEmail} from './form/InputEmail';
-export {default as InputPassword} from './form/InputPassword';
-export {default as Modal} from './modal/Modal';
 export {default as DescriptionTitle} from './empty/DescriptionTitle.layout';
 export {default as TablePagination} from './table-pagination/TablePagination';
-export {default as DrawerActions} from './drawer/DrawerActions';
-export {default as Tabs} from './tabs/Tabs';
-export {default as TabPane} from './tabs/TabPane';
-export {default as Logo} from './logo/Logo';
 
 /* Кнопка */
 export {default as Button} from './button/Button';
@@ -27,3 +12,26 @@ export {default as Button} from './button/Button';
 export {default as Avatar} from './avatar/Avatar';
 /* Input Number Stepper */
 export {default as Stepper} from './stepper/Stepper';
+/* Фиолетовое лого */
+export {default as Logo} from './logo/Logo';
+/* Табы */
+export {default as Tabs} from './tabs/Tabs';
+export {default as TabPane} from './tabs/TabPane';
+/* Drawer */
+export {default as Drawer} from './drawer/Drawer';
+export {default as DrawerActions} from './drawer/DrawerActions';
+/* Modal */
+export {default as Modal} from './modal/Modal';
+/* Modal Menu */
+export {default as ModalMenu} from './modal-menu/ModalMenu';
+/* Card */
+export {default as Card} from './card/Card';
+/* Inputs */
+export {default as FormItem} from './form/form-item/FormItem';
+export {default as InputPhoto} from './form/InputPhoto';
+export {default as InputEmail} from './form/InputEmail';
+export {default as InputPassword} from './form/InputPassword';
+/* Загрузка на всю страницу */
+export {default as Loader} from './loader/Loader';
+/* Загрука блока */
+export {default as LoadingBlock} from './loading-block/LoadingBlock';
